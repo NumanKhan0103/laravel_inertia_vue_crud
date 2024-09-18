b7f71b8a29219d2d837e56ce9355553788fad0ee
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-09-19 00:35:14 +0500

    show active link if user on that page

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -15,15 +15,15 @@
 
       <nav class="flex">
         <div class="space-x-6">
-          <Link :href="route('home')" class="nav-link">Home</Link>
+          <Link :href="route('home')" class="nav-link" :class="{'bg-slate-700' : $page.component === 'Home'}">Home</Link>
         </div>
         <div v-if="$page.props.auth.user" class="space-x-6">
-          <Link :href="route('dashboard')" class="nav-link" >Dashboard</Link>
+          <Link :href="route('dashboard')" class="nav-link"  :class="{'bg-slate-700' : $page.component === 'Admin/Dashboard'}">Dashboard</Link>
           <Link :href="route('logout')" class="nav-link" method="POST" as="button">Logout</Link>
         </div>
         <div v-else class="space-x-6">
-          <Link :href="route('register')" class="nav-link">Register</Link>
-          <Link :href="route('login')" class="nav-link">Login</Link>
+          <Link :href="route('register')" class="nav-link"  :class="{'bg-slate-700' : $page.component === 'Auth/Register'}">Register</Link>
+          <Link :href="route('login')" class="nav-link"  :class="{'bg-slate-700' : $page.component === 'Auth/Login'}">Login</Link>
         </div>
       </nav>
 
