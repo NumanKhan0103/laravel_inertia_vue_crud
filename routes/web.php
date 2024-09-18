3507b4d7ee359234e84a34012959352c933f76c7
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-09-19 00:38:55 +0500

    apply middware on route

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
-Route::inertia('/', 'Home')->name('home');
-
-Route::inertia('/register', 'Auth/Register')->name('register');
-
-Route::post('/register', [AuthController::class, 'register']);
-
-// login
-Route::inertia('/login', 'Auth/Login')->name('login');
-
-Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
 
+    Route::inertia('/dashboard', 'Admin/Dashboard')->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
+Route::middleware(['guest'])->group(function () {
 
-Route::inertia('/dashboard', 'Admin/Dashboard')->name('dashboard');
+    // login
+    Route::inertia('/login', 'Auth/Login')->name('login');
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::inertia('/', 'Home')->name('home');
+
+    Route::inertia('/register', 'Auth/Register')->name('register');
+
+    Route::post('/register', [AuthController::class, 'register']);
+});
